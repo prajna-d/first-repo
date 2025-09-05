746ea97e1c5571d9375f03cdbbb63d27bc8c23a7
prajna-d <pdhinakaran3@gatech.edu>
2025-09-05 13:23:49 -0400

    bug fix for find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -5,7 +5,7 @@ subject_file=$2
 output_file=$3
 
 #tblastn: protein query, nucleotide subject
-tblastn -query "$query_file" -subject "$subject_file" -outfmt "6 pident length qlen slen" | awk '$1>=30 && $2>=($3*0.9){print $0}' > "$output_file"
+tblastn -query "$query_file" -subject "$subject_file" -outfmt "6 pident length qlen slen" | awk '$1>30 && $2>($3*0.9){print $0}' > "$output_file"
 
 #Print the number of matches identified to stdout
 awk 'END{print NR}' "$output_file"
