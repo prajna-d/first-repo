8879784d585075ea99cd5e29ed160bf7e98690f6
prajna-d <pdhinakaran3@gatech.edu>
2025-09-05 10:03:24 -0400

    finished script

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,3 +4,8 @@ query_file=$1
 subject_file=$2
 output_file=$3
 
+#tblastn: protein query, nucleotide subject
+tblastn -query "$query_file" -subject "$subject_file" -outfmt "6 pident length qlen slen" | awk '$1>=30 && $2>=($3*0.9){print $0}' > "$output_file"
+
+#Print the number of matches identified to stdout
+awk 'END{print NR}' "$output_file"
